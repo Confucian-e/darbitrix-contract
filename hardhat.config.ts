263a1f75394becfae4a624a0a2bb8d166c4e7e50
confucian <confucian.e@gmail.com>
2024-03-29 21:19:49 +0800

    chore: update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,28 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-abi-exporter";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+    },
+  },
+  abiExporter: {
+    path: "./abi",
+    runOnCompile: true,
+    only: ["IArbitrage"],
+    format: "json",
+  },
 };
 
 export default config;
